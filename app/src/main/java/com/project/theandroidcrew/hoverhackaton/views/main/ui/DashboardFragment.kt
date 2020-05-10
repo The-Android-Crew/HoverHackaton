fc201bc8168f576b975e53bd1085d480d0454ffa
Robin <emeka14real@gmail.com>
2020-05-10 19:34:18 +0100

    Clean Up

--- app/src/main/java/com/project/theandroidcrew/hoverhackaton/views/main/ui/DashboardFragment.kt
+++ app/src/main/java/com/project/theandroidcrew/hoverhackaton/views/main/ui/DashboardFragment.kt
@@ -5,20 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.navigation.fragment.findNavController
 
 import com.project.theandroidcrew.hoverhackaton.R
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class DashboardFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        frag_dashboard_funds_transfer_cardView.setOnClickListener {
+            findNavController().navigate(R.id.action_dashboardFragment_to_fundsTransferFragment)
+        }
+    }
 }
--- app/src/main/java/com/project/theandroidcrew/hoverhackaton/views/main/ui/FundTransfer.kt
+++ app/src/main/java/com/project/theandroidcrew/hoverhackaton/views/main/ui/FundTransfer.kt
@@ -13,11 +13,7 @@ import com.project.theandroidcrew.hoverhackaton.R
  */
 class FundTransfer : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_fund_transfer, container, false)
     }
 
