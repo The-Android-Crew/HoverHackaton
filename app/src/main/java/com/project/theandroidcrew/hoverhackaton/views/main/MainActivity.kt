aad11b6d85cd183cb756c6d08fc21a4f49fdfda8
Robin <emeka14real@gmail.com>
2020-05-10 08:06:28 +0100

    Finishing touches

--- app/src/main/java/com/project/theandroidcrew/hoverhackaton/views/main/MainActivity.kt
+++ app/src/main/java/com/project/theandroidcrew/hoverhackaton/views/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.project.theandroidcrew.hoverhackaton.views.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.project.theandroidcrew.hoverhackaton.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,4 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
--- app/src/main/java/com/project/theandroidcrew/hoverhackaton/views/main/ui/OTPFragment.kt
+++ app/src/main/java/com/project/theandroidcrew/hoverhackaton/views/main/ui/OTPFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.project.theandroidcrew.hoverhackaton.R
+import kotlinx.android.synthetic.main.fragment_otp.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,9 +21,17 @@ class OTPFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        buttonVerify.setOnClickListener{
+            findNavController().navigate(R.id.action_firstFtagment_to_dashboardFragment)
+        }
+
+        textViewMiniDescription3.setOnClickListener{
+            // Resend OTP
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
     }
+
 }
